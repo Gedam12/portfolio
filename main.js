fb9b079baeb4721da2b39882968ecc962c5ec61d
SayaliGedam <91810513+Gedam12@users.noreply.github.com>
2024-02-07 01:55:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new typed(".text", {
-    Strings: ["Coder", "Programmer", "Web Developer"],
+    Strings: ["Backend", "Programmer", "Web Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
